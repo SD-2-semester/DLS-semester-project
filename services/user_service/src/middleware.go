3570e95b415b608d73e11303cf58be3d16307448
mslaursen <martin_laursen21@hotmail.com>
2024-03-17 19:30:25 +0100

    log

--- services/user_service/src/middleware.go
+++ services/user_service/src/middleware.go
@@ -7,7 +7,7 @@ import (
 
 func LogMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Request: %s %s\n", r.Method, r.URL.Path)
+		log.Printf("Request: %s %s %s\n", r.Method, r.URL.Path, r.RemoteAddr)
 		h.ServeHTTP(w, r)
 	})
 }
