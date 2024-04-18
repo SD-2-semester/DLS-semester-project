c57d881fdebb60ac98193ff6d3dbd66fd2bd64bd
Moshizzl3 <mohamedibr@gmail.com>
2024-04-18 18:27:08 +0200

    removed redundant code and some cleaning

--- services/notification_service/api/routes/notifications/router.py
+++ services/notification_service/api/routes/notifications/router.py
@@ -7,30 +7,21 @@ router = APIRouter()
 
 
 @router.websocket("/notification/{user_id}")
-async def websocket_endpoint(websocket: WebSocket, user_id: str):
+async def websocket_endpoint(websocket: WebSocket, user_id: str) -> None:
     """Connect user by user_id."""
-    await ws_manager.connect_user(user_id, websocket)  # Connect the user
-    message = {
-        "user_id": user_id,
-        "message": f"User {user_id} connected.",
-    }
-    await ws_manager.send_notification(
-        user_id, json.dumps(message)
-    )  # Send connection notification
+
+    # Connect the user
+    await ws_manager.connect_user(user_id, websocket)
+
     try:
         while True:
-            data = await websocket.receive_text()  # Receive message from the user
+            # Receive message from the user
+            data = await websocket.receive_text()
+
             message = {"user_id": user_id, "message": data}
-            await ws_manager.send_notification(
-                user_id, json.dumps(message)
-            )  # Broadcast user message
+
+            # Broadcast user message
+            await ws_manager.send_notification(user_id, json.dumps(message))
 
     except WebSocketDisconnect:
-        await ws_manager.disconnect_user(user_id, websocket)  # Disconnect the user
-        message = {
-            "user_id": user_id,
-            "message": f"User {user_id} disconnected.",
-        }
-        await ws_manager.send_notification(
-            user_id, json.dumps(message)
-        )  # Notify about user disconnection
+        await ws_manager.disconnect_user(user_id, websocket)
